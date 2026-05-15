a3dc82fb560546496a6d1ebde6a5b18a3ffaa3a4
ryanndraa-coder <ryanndraa@gmail.com>
2026-05-15 17:23:08 +0700

    debug simple

--- config/database.php
+++ config/database.php
@@ -3,16 +3,5 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host = getenv('MYSQLHOST');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$port = getenv('MYSQLPORT');
-
-$db = getenv('MYSQLDATABASE');
-
-$conn = @new mysqli($host, $user, $pass, $db, $port);
-
-if ($conn->connect_error) {
-    die("Database Error: " . $conn->connect_error);
-}
+echo "DATABASE FILE LOADED";
 ?>
--- login.php
+++ login.php
@@ -3,22 +3,5 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "STEP 1<br>";
-
-session_start();
-
-echo "STEP 2<br>";
-
-require_once __DIR__ . '/config/database.php';
-
-echo "STEP 3 DATABASE CONNECTED<br>";
-
-$query = $conn->query("SELECT * FROM user");
-
-echo "STEP 4 QUERY OK<br>";
-
-$data = $query->fetch_assoc();
-
-echo "<pre>";
-print_r($data);
-echo "</pre>";
+echo "LOGIN PAGE WORKING";
+?>
