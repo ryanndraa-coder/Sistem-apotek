d9b3c2273768ce06f9ef46e939aef2fa6671a893
ryanndraa-coder <ryanndraa@gmail.com>
2026-05-15 17:42:18 +0700

    fix railway

--- config/database.php
+++ config/database.php
@@ -3,5 +3,15 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "DATABASE FILE LOADED";
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("Database Error: " . $conn->connect_error);
+}
 ?>
