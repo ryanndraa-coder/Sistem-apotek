fe0dc4c06645d19a45e38e7ffc5628beb8b9a851
ryanndraa-coder <ryanndraa@gmail.com>
2026-05-15 16:35:54 +0700

    debug login

--- login.php
+++ login.php
@@ -1,3 +1,11 @@
 <?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+echo "LOGIN PAGE OK <br>";
+
+session_start();
+require_once __DIR__ . '/config/database.php';
+
+echo "DATABASE CONNECTED <br>";
